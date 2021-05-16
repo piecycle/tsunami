b7dfcddcf6a361a035da01e468d72a40c6671082
piecycle <pinner.michael@gmail.com>
2021-05-16 20:56:52 +0200

    v0.6.1 Support for UAM button based on the insulin button (work in progress - dummy button) Based on v0.5 including mods by MT (dynamic target) W2 condition: Minimum IOB is based on max_IOB * user settable variable (iob_scale) Added scale_min (ISF offset for more aggressive action) Data smoothing function for W2 assist; Cleaned up SMB preferences Modified percentage parameters (now using true percentages instead of fractions of 1) Added excel sheet for illustration of the scaling algorithm (see repository) Updated README file

--- app/src/main/java/info/nightscout/androidaps/dialogs/TempTargetDialog.kt
+++ app/src/main/java/info/nightscout/androidaps/dialogs/TempTargetDialog.kt
@@ -282,7 +282,7 @@ class TempTargetDialog : DialogFragmentWithDate() {
                                 }
 
                             })
-                            //}
+                            }
                         }
 
                     }
